237f5ae80ddfe1982458352051c91f21afd2edb0
gadget <v.stefanovich92@gmail.com>
2023-08-23 23:40:28 +0300

    Added migrations for Catalog app

--- enigma_test_app/Catalog/models.py
+++ enigma_test_app/Catalog/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 
-class Filial(models.Model):
-    name = models.CharField(max_length=255)
-    region = models.IntegerField()
-    filial_price = models.ForeignKey("Filial", on_delete=None)
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     characteristic = models.ManyToManyField("Characteristic")
 
-class FilialPrice(models.Model):
-    product = models.ForeignKey(Product, on_delete=None)
-    price = models.IntegerField()
+
+class Filial(models.Model):
+    name = models.CharField(max_length=255)
+    region = models.IntegerField()
+
 
 class Characteristic(models.Model):
-    self = models.ForeignKey("Characteristic", on_delete=None)
+    self = models.ForeignKey("Characteristic", on_delete=models.DO_NOTHING)
     name = models.CharField(max_length=255)
+
+
+class FilialPrice(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    filial = models.ForeignKey(Filial, on_delete=models.DO_NOTHING)
+    price = models.IntegerField()
