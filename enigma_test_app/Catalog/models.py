42b2db8aee641403f59bd2da24abb60e5e9b39fe
gadget <v.stefanovich92@gmail.com>
2023-08-23 23:15:02 +0300

    Fix errors in repo. Added models.

--- enigma_test_app/Catalog/models.py
+++ enigma_test_app/Catalog/models.py
@@ -1,22 +1,18 @@
 from django.db import models
 
-
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-
-
 class Filial(models.Model):
     name = models.CharField(max_length=255)
     region = models.IntegerField()
+    filial_price = models.ForeignKey("Filial", on_delete=None)
 
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    characteristic = models.ManyToManyField("Characteristic")
+
+class FilialPrice(models.Model):
+    product = models.ForeignKey(Product, on_delete=None)
+    price = models.IntegerField()
 
 class Characteristic(models.Model):
     self = models.ForeignKey("Characteristic", on_delete=None)
     name = models.CharField(max_length=255)
-    product = models.ManyToManyField(Product)
-
-
-class FilialPrice(models.Model):
-    product = models.ForeignKey(Product, on_delete=None)
-    filial = models.ForeignKey(Filial, on_delete=None)
-    price = models.IntegerField()
